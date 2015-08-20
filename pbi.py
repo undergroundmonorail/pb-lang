889e3af1d00a476b96e57fad450b11d97edba290
undergroundmonorail <undergroundmonorail@gmail.com>
2015-08-20 04:50:29 -0400

    i think execution works
    i have no way to view the output yet but i think the right values are on
    the canvas

--- pbi.py
+++ pbi.py
@@ -2,6 +2,17 @@
 
 import sys
 
+# Some might say using global variables for this purpose is bad practice.
+# I think those people could stand to be a little bit more adventurous.
+
+canvas = { (0, 0):(0, 0)}
+X = 0
+Y = 0
+P = 0
+B = 0
+C = 0
+T = 0
+
 def read_code(file):
 	"""Strip comments and meaningless characters"""
 	instructions = ('XYPBCT'
@@ -46,14 +57,72 @@ def tokenize(code):
 				tokens[i-1:i+1] = [tokens[i-1] + t]
 	return tokens
 
-def execute(tokens, canvas=None, X=0, Y=0, P=0, B=0, C=0, T=0):
-	pass
+def canvas_read(x, y):
+	if (x, y) not in canvas:
+		canvas[(x,y)] = (0, 0)
+	return canvas[(x,y)]
+
+def update():
+	"""Update the B and C variables"""
+	global B
+	global C
+	B, C = canvas_read(X, Y)
+
+def expression(e):
+	"""Return the result of an expression"""
+	# TODO: make this not a piece of shit
+	return eval(e, globals())
+	
+def execute(tokens):
+	global X # l
+	global Y # m
+	global P # a
+	global T # o
+	
+	global canvas
+	
+	for token in tokens:
+		if token in '^v<>':
+			token += '[1]'
 		
+		if token[0] == '^':
+			Y -= expression(token[2:-1])
+			update()
+		elif token[0] == 'v':
+			Y += expression(token[2:-1])
+			update()
+		elif token[0] == '<':
+			X -= expression(token[2:-1])
+			update()
+		elif token[0] == '>':
+			X += expression(token[2:-1])
+			update()
+		elif token[0] == 't':
+			T = expression(token[2:-1])
+		elif token[0] == 'b':
+			canvas[(X, Y)] = (expression(token[2:-1]), P)
+			update()
+		elif token == 'c':
+			P = (P + 1) % 8
+		elif token[0] == 'w':
+			condition = token.split('[')[1].split(']')[0]
+			break_on_not = '=' in condition
+			condition_1, condition_2 = condition.split('!='[break_on_not])
+			code = tokenize(token[4+len(condition):-1])
+			while (expression(condition_1) == expression(condition_2) and break_on_not) or (expression(condition_1) != expression(condition_2) and not break_on_not):
+				execute(code)
+
+def get_input():
+	global canvas
+	for x, c in enumerate(raw_input('Enter input: ')):
+		canvas[(x, -1)] = (ord(c), 0)
 
 def main(file):
+	get_input()
 	code = read_code(file)
 	tokens = tokenize(code)
-	
+	execute(tokens)
+
 if __name__ == '__main__':
 	if len(sys.argv) == 2:
 		main(sys.argv[1])
