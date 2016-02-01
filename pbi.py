7f9d35d4b73766291f6cc6ee4f64608c0456e81c
undergroundmonorail <undergroundmonorail@gmail.com>
2016-01-31 21:40:51 -0500

    big update!
    ability to enter more than one line of text for input, no longer prompts
    for input if the program doesn't look for it, shows where the brush is
    in visual mode, maybe some other stuff i forgot

--- pbi.py
+++ pbi.py
@@ -77,6 +77,17 @@ def tokenize(code):
 	return tokens
 
 def canvas_read(x, y):
+	# Only ask for input the first time x>=0, y=-1 is read
+	if y == -1 and x >= 0:
+		get_input()
+		
+		def no_input_check(x,y):
+			if (x, y) not in canvas:
+				canvas[(x,y)] = (0, 0)
+			return canvas[(x,y)]
+		
+		canvas_read.__code__ = no_input_check.__code__
+	
 	if (x, y) not in canvas:
 		canvas[(x,y)] = (0, 0)
 	return canvas[(x,y)]
@@ -93,9 +104,6 @@ def expression(e):
 	return eval(e, globals())
 	
 def execute(tokens, delay):
-	if delay != 0:
-		output(delay)
-	
 	global X # l
 	global Y # m
 	global P # a
@@ -104,6 +112,9 @@ def execute(tokens, delay):
 	global canvas
 	
 	for token in tokens:
+		if delay != 0:
+			output(delay)
+			
 		if token in '^v<>':
 			token += '[1]'
 		
@@ -134,57 +145,118 @@ def execute(tokens, delay):
 			while (expression(condition_1) == expression(condition_2) and break_on_not) or (expression(condition_1) != expression(condition_2) and not break_on_not):
 				execute(code, delay)
 
+curr_colour = 7
+
 def get_input():
 	global canvas
-	for x, c in enumerate(raw_input('Enter input: ')):
+	
+	if sys.stdin.isatty():
+		# interactive input
+		if curr_colour != 7:
+			sys.stdout.write('\033[' + str(37) + 'm')
+		print "Enter program's input. Empty line to terminate."
+		
+		user_input = '\n'.join(iter(raw_input, ''))
+		
+		if curr_colour != 7:
+			sys.stdout.write('\033[' + str(30 + [7, 2, 3, 4, 5, 6, 0][curr_colour]) + 'm')
+	else:
+		user_input = sys.stdin.read()
+	
+	for x, c in enumerate(user_input):
 		canvas[(x, -1)] = (ord(c), 0)
 
-def output(delay):
+def output(delay, done=False):
 	if delay != 0:
-		print '\033[2J\033[1;1f'
+		sys.stdout.write('\033[2J\033[1;1f')
+	
 	for key in canvas.keys():
 		if canvas[key] == (0, 0):
 			del canvas[key]
-	max_x = max(map(lambda t:t[0], canvas.keys()))
-	max_y = max(map(lambda t:t[1], canvas.keys()))
+	
+	global curr_colour
+	curr_colour = 7
+	
+	try:max_y = max(map(lambda t:t[1], canvas.keys()) + [1])
+	except:max_y = 1
+	
 	for row in xrange(max_y + 1):
+		try:max_x = max(map(lambda t:t[0], filter(lambda t:t[1] == row, canvas.keys())))
+		except:max_x = 0
+		
 		o = []
 		for column in xrange(max_x + 1):
 			char, colour = canvas_read(column, row)
 			char = char or 32
 			o.append((char, colour))
+		if o == [(32, 0)]:
+			o = []
+		
 		if RAW:
 			print o
 		else:
+			while delay != 0 and row == Y and len(o)-1 < X and not done:
+				o.append((32, o[-1][1]) if o else (32, [7, 1, 2, 3, 4, 5, 6, 0][curr_colour]))
+			
+			buffer = ''
 			for i, c in enumerate(o):
-				if c[1] == 0: o[i] = (c[0], 7)
-				elif c[1] == 7: o[i] = (c[0], 0)
-			print ''.join('\033[' + str(c[1]+30) + 'm' + chr(c[0]) for c in o)
+				c = (c[0], [7, 1, 2, 3, 4, 5, 6, 0][c[1]])
+				if c[1] != [7, 1, 2, 3, 4, 5, 6, 0][curr_colour]:
+					buffer += '\033[' + str(c[1]+30) + 'm'
+					curr_colour = [7, 1, 2, 3, 4, 5, 6, 0][c[1]]
+				if row == Y and X == i and delay != 0 and not done:
+					buffer += colorama.Back.RED + chr(c[0]) + colorama.Back.RESET
+				else:
+					buffer += chr(c[0])
+			print buffer
+		
+	while delay != 0 and row < Y and not done:
+		row += 1
+		if row == Y:
+			column = 0
+			while column < X:
+				sys.stdout.write(' ')
+				column += 1
+			print colorama.Back.RED + ' ' + colorama.Back.RESET
+		else: print
 	
+	if curr_colour != 7: sys.stdout.write('\033[' + str(37) + 'm')
 	if delay != 0:
 		time.sleep(delay / 1000.0)
 		
 				
 
 def main(args):
-	file = args[0]
+	global RAW
+	
 	delay = 0
-	if len(args) == 2:
-		if file.startswith("-d="):
-			delay = int(file[3:])
-			file = args[1]
-		else:
-			delay = int(args[1][3:])
+	for i, arg in enumerate(args):
+		if arg.startswith("-d="):
+			delay = int(arg[3:])
+			args.pop(i)
+			break
+	for i, arg in enumerate(args):
+		if arg.startswith('-r'):
+			RAW = True
+			args.pop(i)
+			break
+	
+	if len(args) != 1:
+		exit('Too many arguments!!')
+	
+	file = args[0]
+	
 	if not RAW:
 		colorama.init()
-	get_input()
 	code = read_code(file)
 	tokens = tokenize(code)
 	execute(tokens, delay)
-	output(delay)
+	if delay != 0:
+		sys.stdout.write('\033[2J\033[1;1f')
+	output(0, True)
 
 if __name__ == '__main__':
-	if 4 > len(sys.argv) > 1:
+	if len(sys.argv) > 1:
 		main(sys.argv[1:])
 	else:
 		exit('Usage: pbi.py program.pb')
